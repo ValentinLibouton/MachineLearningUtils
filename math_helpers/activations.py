d1d4ca4e752cf7582a6183a01ff0f024a146695e
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2024-03-30 12:28:17 +0100

    add some functions of activation

--- math_helpers/activations.py
+++ math_helpers/activations.py
@@ -5,5 +5,25 @@ def sigmoid(x):
   return 1 / (1 + tf.math.exp(-x))
 
 
-  def relu(x):
+def relu(x):
   return tf.math.maximum(0, x)
+
+
+def softmax(x):
+  return tf.nn.softmax(x)
+
+
+def tanh(x):
+    return tf.math.tanh(x)
+
+
+def leaky_relu(x, alpha=0.01):
+    return tf.where(x > 0, x, alpha * x)
+
+
+def elu(x, alpha=1.0):
+    return tf.where(x > 0, x, alpha * (tf.math.exp(x) - 1))
+
+
+def swish(x):
+    return x * sigmoid(x)
