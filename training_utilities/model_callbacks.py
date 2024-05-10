0b86b63865abf4b31e38aa2b00e25d1f1604e1bd
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2024-05-10 17:40:16 +0200

    slight modifications to the arguments

--- training_utilities/model_callbacks.py
+++ training_utilities/model_callbacks.py
@@ -19,31 +19,28 @@ def create_tensorboard_callback(dir_name, experiment_name, update_freq='epoch'):
     return tensorboard_callback
 
 
-def create_model_checkpoint(dir_name, experiment_name, monitor="val_loss", save_best_only=True, verbose=0):
+def create_model_checkpoint(model_name, dir_name="model_experiments", monitor="val_loss", save_best_only=True, verbose=0):
     """
     Create a callback for a TensorFlow Keras model that saves the model as a checkpoint.
 
-    The callback saves the model at a specified directory and under a specific experiment name. The model
-    is saved based on the monitoring of a certain metric (default is 'val_loss') and can be configured to save
-    only the best performing model.
+    The callback saves the model at a specified directory with a given model name. It monitors a specified metric
+    (default is 'val_loss') and can be configured to save only the best performing model based on this metric.
 
     Args:
-        dir_name (str): The directory name where the model checkpoint will be saved.
-        experiment_name (str): The name of the experiment under which the model checkpoint will be saved.
+        model_name (str): The name of the model under which the checkpoint will be saved.
+        dir_name (str, optional): The directory name where the model checkpoint will be saved. Defaults to "model_experiments".
         monitor (str, optional): The metric name to monitor. Defaults to 'val_loss'.
-        save_best_only (bool, optional): Whether to save only the model that achieved the best
-            performance on the monitored metric. Defaults to True.
-        verbose (int, optional): Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-            Defaults to 0.
+        save_best_only (bool, optional): Whether to save only the model that achieved the best performance on the monitored metric. Defaults to True.
+        verbose (int, optional): Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch. Defaults to 0.
 
     Returns:
         ModelCheckpoint: A TensorFlow Keras callback that can be used in model training to save the model.
 
     Example:
-        checkpoint_callback = create_model_checkpoint("models", "exp1")
+        checkpoint_callback = create_model_checkpoint("my_model")
         model.fit(x_train, y_train, callbacks=[checkpoint_callback])
     """
-    filepath = Path(dir_name) / experiment_name
+    filepath = Path(dir_name) / model_name
     return tf.keras.callbacks.ModelCheckpoint(filepath=filepath,
                                               monitor=monitor,
                                               verbose=verbose,
